5eb632d38a4fcd61901f91b632fb4e14c51db315
hiranaz <161939391+hiraham@users.noreply.github.com>
2024-06-02 17:59:42 +0500

    Add files via upload

---
+++ for-loop.ts
@@ -0,0 +1,48 @@
+//loops can execute a block of code a number of times.
+//multiple loops
+//foe(bhot dafa chala sakte han)
+//for/in -
+
+//while (phle pochen ge)
+    //for loop (kam karne k bad ponche ga)
+ 
+//expective loop or unexpective loops
+//class
+// basic syntax of the loop
+//for(initialization; condition;incriment/decrement){
+//    statement
+//}
+
+//initialization means where the loop start
+//condition is used to check if the condition
+//increment/decrement the condition
+
+//for(let i=1; i <=1 ; i++){
+ //   console.log(i);
+
+//for(let i = 1; i <= 10; i++){
+ //   let tableNumber: number = 9;
+ //   console.log(tableNumber, "x" , i ,"=" , tableNumber* i);
+
+//
+for(let i = 1; i <= 10 ; i++){
+    let tableNumber: number = 5
+    console.log(tableNumber, "x" , i ,"=" , tableNumber*i);
+}
+
+
+/*for(let i = 0; i<=10; i++){
+    console.log(i,"a+b=10")
+*/
+//let datalength = 4
+//for(let i = 0; i <  datalength; i++){
+ ////   console.log(datalength)
+//}
+
+
+
+
+
+
+
+
