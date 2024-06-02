ac0777f44949ff9b28ac03aad37a640062c37838
hiranaz <161939391+hiraham@users.noreply.github.com>
2024-06-02 17:56:36 +0500

    Add files via upload

---
+++ data-type.ts
@@ -0,0 +1,9 @@
+let myAge:number= 25;
+let myName:string ="hira ham";
+let isJobless:boolean = false;
+console.log(myAge, myName, isJobless);
+console.log(myName.toUpperCase());//HIRA HAM 
+
+  
+
+ 
