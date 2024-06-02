14e95cabea6273e0f9c09a5b41478e44628f4914
hiranaz <161939391+hiraham@users.noreply.github.com>
2024-06-02 18:00:56 +0500

    Add files via upload

---
+++ for-of.ts
@@ -0,0 +1,21 @@
+const girls = ["sana","sara","ayesha","bisma"];
+let index = 1
+for(let girl of girls){
+    console.log(`my ${index} sister is ${girl}`);
+}
+console.log('-------------------')
+//loops on an arry
+
+const myClothes : string[] = ["t_shirt", "sweater","paint","shoes","soacks "];
+
+for(const item of myClothes){
+    console.log(item);
+}
+
+console.log('-------------------')
+
+//loop on strings
+
+for(const alphabet of'hello'){
+    console.log(alphabet);
+}
