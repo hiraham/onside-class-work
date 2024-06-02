611b4c19bf799e522dfe62ad951fca9eac72e7b9
hiranaz <161939391+hiraham@users.noreply.github.com>
2024-06-02 17:50:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("hello world! my name is hira");
+
+
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
