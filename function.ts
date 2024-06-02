8c06f7b64d474c2d8e199ed6e1cb3ec41dc028cb
hiranaz <161939391+hiraham@users.noreply.github.com>
2024-06-02 18:02:38 +0500

    Add files via upload

---
+++ function.ts
@@ -0,0 +1,38 @@
+// let tea = 2 +2+ 2;
+//function makeTea():number{
+  //  let tea = 2 + 2 +2;
+  //  console.log(tea);
+ //   return tea
+//}
+//call me
+//maketea();
+//let response =makeTea();
+//console.log(response)
+
+/*let biryani = 1 + 2 + 3 + 5 + 1 ;
+function makebiryani(){
+  console.log(biryani);
+  return biryani;
+}
+let response = makebiryani();
+console.log(response)
+
+
+let tea = 2 +2 + 2
+function maketea (suger =4 , milk =2, water?):void{                  //perameter
+    let tea = 2 + suger + milk + water
+    console.log(tea);
+}
+maketea(undefined,2)//arguments
+maketea(1,3)//arguments*/
+
+//let idCard = name,rollNumber,courseName
+function makeidCard(name:string,rollNumber:number ,course: string):void { 
+        console.log( "name" + name)
+        console.log( "rollNumber" + rollNumber),
+        console.log( "courseName" + course)
+        return "idCard"
+           };
+     
+       let response = ("name=hira, rollNumber=3333, course=phython");
+       console.log(response)
