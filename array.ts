b2abe6d41a0de244968d185914d0532813c4e3ca
hiranaz <161939391+hiraham@users.noreply.github.com>
2024-06-02 17:58:02 +0500

    Add files via upload

---
+++ array.ts
@@ -0,0 +1,46 @@
+//const card []=["wheat","rice","oil"];//square braket
+//console.log(card);
+//const myContacts:Array<string>=["hira","hammad","naz"];
+//console .log(myContacts);
+//console.log("-----------")
+
+                    /////    0      1       2
+const myLanchBox:Array<string>=["biscuit","daal chawal","juice"];
+
+console .log("here is my card third element",myLanchBox[2] );
+console.log("before changing",myLanchBox);
+
+myLanchBox[2]="daal chawal";//this syntax is chaged in the 
+myLanchBox[3]="CUP CAKE";//this syntax add aelement in arraymyLanchBox);
+
+console.log("after changing",myLanchBox);
+
+console.log("--------");
+
+console.log("length of myLanchBox" ,myLanchBox);
+
+console.log("------------");
+
+myLanchBox.push = ("samosa");//this syntax remove element from arraymyLanchBox
+
+console.log("myLanchBox");
+
+myLanchBox.pop() ;
+console.log("myLanchBox");
+
+
+const  mywifes: readonly string[]=["wife1","wife2",];
+//mywifes.push("wifes3");//give error
+//mywifes[3]//error
+console.log("mywifes");
+
+
+
+
+
+
+
+
+
+
+
